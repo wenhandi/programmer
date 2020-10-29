86e4493ba08659cdab351c478e064575e9b012fd
xupan <18233566286@163.com>
2020-10-29 14:27:25 +0800

    公众号

---
+++ h5/weChatSubscription/book.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="box">
+    <div>
+      <div>书名</div>
+      <div>作者</div>
+      <div>正</div>
+    </div>
+    <div v-for="item in list" :key="item.title">
+      <div>{{item.title}}</div>
+      <div>{{item.author}}</div>
+      <div>{{item.zheng}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        },
+        {
+          title: "《财富自由之路》",
+          author: "李笑来",
+          zheng: "3"
+        }
+      ]
+    };
+  },
+  methods: {},
+  created() {}
+};
+</script>
+<style scoped>
+</style>
+
